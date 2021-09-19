6aa170aac7fc8a95c8f7a7bd7218cf9d8c93eb54
Gabriel Amador <gabrielramador2014@gmail.com>
2021-09-19 19:15:02 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
+import { FiUser, FiCalendar } from 'react-icons/fi';
+import styles from '../styles/pages/home.module.scss';
+
+// import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
 
 interface Post {
   uid?: string;
@@ -24,10 +26,131 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
-  // TODO
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const a = postsPagination?.next_page;
 
-  return <h1>Hello SpaceTraveling</h1>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Posts | SpaceTraveling</title>
+      </Head>
+
+      <div className={styles.content}>
+        <header>
+          <img src="./logo.svg" alt="logo" />
+        </header>
+
+        <section className={styles.postsContainer}>
+          <div className={styles.post}>
+            <h2>
+              <a href="/"> How to use Hooks </a>
+            </h2>
+            <p className={styles.summary}>
+              Thinking in sincronization instead life cycles.
+            </p>
+
+            <div>
+              <span>
+                <FiCalendar size={20} />
+                <time>15 Mar 2021</time>
+              </span>
+
+              <span>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.post}>
+            <h2>
+              <a href="/"> How to use Hooks </a>
+            </h2>
+            <p className={styles.summary}>
+              Thinking in sincronization instead life cycles.
+            </p>
+
+            <div>
+              <span>
+                <FiCalendar size={20} />
+                <time>15 Mar 2021</time>
+              </span>
+
+              <span>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.post}>
+            <h2>
+              <a href="/"> How to use Hooks </a>
+            </h2>
+            <p className={styles.summary}>
+              Thinking in sincronization instead life cycles.
+            </p>
+
+            <div>
+              <span>
+                <FiCalendar size={20} />
+                <time>15 Mar 2021</time>
+              </span>
+
+              <span>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.post}>
+            <h2>
+              <a href="/"> How to use Hooks </a>
+            </h2>
+            <p className={styles.summary}>
+              Thinking in sincronization instead life cycles.
+            </p>
+
+            <div>
+              <span>
+                <FiCalendar size={20} />
+                <time>15 Mar 2021</time>
+              </span>
+
+              <span>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.post}>
+            <h2>
+              <a href="/"> How to use Hooks </a>
+            </h2>
+            <p className={styles.summary}>
+              Thinking in sincronization instead life cycles.
+            </p>
+
+            <div>
+              <span>
+                <FiCalendar size={20} />
+                <time>15 Mar 2021</time>
+              </span>
+
+              <span>
+                <FiUser size={20} />
+                <p>Joseph Oliveira</p>
+              </span>
+            </div>
+          </div>
+        </section>
+
+        <button type="button"> Carregar mais posts </button>
+      </div>
+    </div>
+  );
 }
 
 // export const getStaticProps = async () => {
